66de33d557f0424adc748de81134771d54fbec85
carlosrivanunes <106713646+carlosrivanunes@users.noreply.github.com>
2024-11-17 22:03:04 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>valeu pelo curso de 2000 reais que eu consegui de graça bro</h1>
+    return <h1>valeu pelo curso de 2000 reais que eu consegui de graça bro.</h1>
 }
 
 export default Home;
